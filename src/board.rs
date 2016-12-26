f93d597939927bc344cdc8209c277b97e7abbfdf
risboo6909 <risboo6909@yandex-team.ru>
2016-12-27 00:58:00 +0300

    Add occupied set and fixes

--- src/board.rs
+++ src/board.rs
@@ -1,16 +1,24 @@
 pub mod board {
 
     use std::cmp::max;
+    use std::collections::HashSet;
+
+    #[derive(PartialEq, Eq, Hash)]
+    pub struct Coord {
+        pub col: usize,
+        pub row: usize,
+    }
 
     pub struct Board {
         cells: Vec<Vec<bool>>,
+        occupied: HashSet<Coord>,
     }
 
     impl Board {
 
         pub fn new(width: usize, height: usize) -> Board {
 
-            // minimum board size if 1x1
+            // minimum board size is 1x1
             let cols = max(width, 1);
             let rows = max(height, 1);
 
@@ -20,7 +28,7 @@ pub mod board {
                 tmp.push(vec![false; cols]);
             }
 
-            Board {cells: tmp}
+            Board {cells: tmp, occupied: HashSet::new()}
 
         }
 
@@ -28,8 +36,6 @@ pub mod board {
 
             // extend board by 1 if needed, no need to extend more
             // because we always scan vicinity of radius 1 of any cell
-
-            // TODO: Check negative indices
 
             if row as isize - self.cells.len() as isize > 0 {
                 panic!("Row index is {} but the number of rows is {}",
@@ -51,14 +57,27 @@ pub mod board {
 
         }
 
-        pub fn set_cell(&mut self, val: bool, col: usize, row: usize) {
+        pub fn born_at(&mut self, col: usize, row: usize) {
             self.ensure_cell(col, row);
-            self.cells[row][col] = val;
+            self.cells[row][col] = true;
+
+            self.occupied.insert(Coord {col: col, row: row});
         }
 
-        pub fn get_cell(&mut self, col: usize, row: usize) -> Option<bool> {
+        pub fn kill_at(&mut self, col: usize, row: usize) {
             self.ensure_cell(col, row);
-            Some(self.cells[row][col])
+            self.cells[row][col] = false;
+
+            self.occupied.remove(&Coord {col: col, row: row});
+        }
+
+        pub fn get_cell(&mut self, col: usize, row: usize) -> bool {
+            self.ensure_cell(col, row);
+            self.cells[row][col]
+        }
+
+        pub fn get_occupied<'a>(&'a self) -> Box<Iterator<Item=&'a Coord> + 'a> {
+            Box::new(self.occupied.iter())
         }
 
     }
@@ -69,27 +88,40 @@ pub mod board {
 #[test]
 fn test_board_ok() {
 
+    use std::collections::HashSet;
+
     let mut my_board = board::Board::new(5, 5);
 
     // set some existing cells
-    my_board.set_cell(true, 0, 0);
-    my_board.set_cell(true, 4, 4);
+    my_board.born_at(0, 0);
+    my_board.born_at(4, 4);
 
     // extend board by one cell
-    my_board.set_cell(true, 5, 2);
+    my_board.born_at(5, 2);
 
     // test allocated cells
-    assert_eq!(my_board.get_cell(0, 0), Some(true));
-    assert_eq!(my_board.get_cell(4, 4), Some(true));
+    assert_eq!(my_board.get_cell(0, 0), true);
+    assert_eq!(my_board.get_cell(4, 4), true);
 
     // test previously expanded cell
-    assert_eq!(my_board.get_cell(5, 2), Some(true));
+    assert_eq!(my_board.get_cell(5, 2), true);
 
     // test existing cell
-    assert_eq!(my_board.get_cell(2, 2), Some(false));
+    assert_eq!(my_board.get_cell(2, 2), false);
 
     // check extended cell
-    assert_eq!(my_board.get_cell(5, 3), Some(false));
+    assert_eq!(my_board.get_cell(5, 3), false);
+
+    my_board.kill_at(0, 0);
+    assert_eq!(my_board.get_cell(0, 0), false);
+
+    let mut expected: HashSet<board::Coord> = HashSet::new();
+
+    expected.insert(board::Coord{col: 4, row: 4});
+    expected.insert(board::Coord{col: 5, row: 2});
+
+    assert_eq!(expected.iter().eq(my_board.get_occupied()), true);
+
 }
 
 #[test]
