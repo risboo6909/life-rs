1000af3d89e98a5cb0028cd530d9377dea7da248
risboo6909 <risboo6909@yandex-team.ru>
2016-12-27 20:12:37 +0300

    Add window creation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 extern crate piston_window;
+
 use piston_window::*;
 
 mod board;
 
 
+enum State {
+    Working,
+    Paused,
+    Help,
+}
+
 fn main() {
+
+    let mut window: PistonWindow = WindowSettings::new(
+        "My Rust Life",
+        [600, 600]
+    )
+    .exit_on_esc(true)
+    .build()
+    .unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+        });
+    }
 
 }
