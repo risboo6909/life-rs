a8dab1b5b85e582df36b51f089a1949a6788e9e7
risboo6909 <risboo6909@yandex-team.ru>
2017-03-11 22:52:47 +0300

    stub methods for SymVecBased

--- src/board/vect/mod.rs
+++ src/board/vect/mod.rs
@@ -1,8 +1,29 @@
 mod symvec;
 
 use self::symvec::SymVec;
-use ::board::{BoardInternal, Cell};
+use ::board::{BoardInternal, Cell, CellIterType};
 
 pub struct SymVecBased {
     cells: SymVec<SymVec<Cell>>
+}
+
+
+impl BoardInternal for SymVecBased {
+
+    fn get_cell(&self, col: isize, row: isize) -> Option<&Cell> {
+        unimplemented!()
+    }
+
+    fn set_cell(&mut self, col: isize, row: isize, val: Cell) {
+        unimplemented!()
+    }
+
+    fn rm_cell(&mut self, col: isize, row: isize) {
+        unimplemented!()
+    }
+
+    fn get_iter<'a>(&'a self) -> Box<Iterator<Item=CellIterType> + 'a> {
+        unimplemented!()
+    }
+
 }
