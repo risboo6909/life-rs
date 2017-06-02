32e410e357325adf0d1399e93ccfce132b6f9ea0
risboo6909 <risboo6909@yandex-team.ru>
2017-06-02 20:06:49 +0300

    Board centering and scale

--- ui/src/windows/board.rs
+++ ui/src/windows/board.rs
@@ -40,6 +40,7 @@ impl GameBoard {
     pub fn new(window: Rc<GraphicsWindow>, engine: Rc<RefCell<Engine>>) -> GameBoard {
 
         GameBoard {
+
             window: window,
             engine: engine,
 
@@ -53,6 +54,51 @@ impl GameBoard {
             last_pos: None,
 
         }
+
+    }
+
+    fn fit_board(&mut self) {
+
+        // fit game board on a screen
+
+        if let Some(board_cols) = self.engine.borrow().get_board().get_cols() {
+            if let Some(board_rows) = self.engine.borrow().get_board().get_rows() {
+
+                // try to make all borders to be visible if possible
+
+                // grid must take 55% of the window
+                let target_area = 0.55 * self.window.get_width() * self.window.get_height();
+
+                // the derivation is as follows:
+                // rows*cols*s*k*w*s*k*h = 0.55*width*height
+                // k^2 = (0.55*width*height) / (rows*cols*s^2*w*h)
+
+                let cell_area = self.cell.get_width(&self.cam) * self.cell.get_height(&self.cam);
+                let current_area = cell_area * (board_cols * board_rows) as f64;
+
+                let scale_factor = (target_area / current_area).sqrt();
+                let scale_coeff = self.cam.get_scale();
+
+                self.cam.set_scale((scale_factor * scale_coeff));
+
+            }
+        }
+
+        // position camera to look at the center of a board
+
+        let (center_x, center_y) = self.get_board_center();
+
+        let delta_x = match(center_x) {
+            Some(x) => x - 0.5 * self.window.get_width(),
+            None => 0.0
+        };
+
+        let delta_y = match(center_y) {
+            Some(y) => y - 0.5 * self.window.get_height(),
+            None => 0.0
+        };
+
+        self.cam.set_pos_delta(-delta_x, -delta_y);
 
     }
 
@@ -211,7 +257,8 @@ impl WindowBase for GameBoard {
 
             &Event::Input(Input::Press(Button::Keyboard(Key::F))) => {
                 // reset camera coordinates to defaults
-                self.cam.reset();
+                //self.cam.reset();
+                self.fit_board();
             }
             // misc controls <-
 
@@ -242,7 +289,7 @@ impl GameBoard {
         // get absolute screen coordinate of left border of a board
         let cols = match self.engine.borrow().get_board().get_cols() {
             Some(cols) => cols,
-            None => (self.window.get_width() / self.cell.get_width(&self.cam)) as u32
+            None => (self.window.get_width() / self.cell.get_width(&self.cam)) as usize
         };
         let x = self.cam.translate_x(self.window.get_half_width() -
             0.5 * cols as f64 * self.cell.get_width(&self.cam));
@@ -254,7 +301,7 @@ impl GameBoard {
         // get absolute screen coordinate of top border of a board
         let rows = match self.engine.borrow().get_board().get_rows() {
             Some(rows) => rows,
-            None => (self.window.get_height() / self.cell.get_height(&self.cam)) as u32
+            None => (self.window.get_height() / self.cell.get_height(&self.cam)) as usize
         };
         let y = self.cam.translate_y(self.window.get_half_height() -
             0.5 * rows as f64 * self.cell.get_height(&self.cam));
@@ -269,6 +316,21 @@ impl GameBoard {
                 0.5 * rows as f64 * self.cell.get_height(&self.cam));
             if rows % 2 == 0 { y - self.cell.get_half_height(&self.cam) } else { y }
         } else { self.window.get_height() }
+    }
+
+    #[inline]
+    fn get_board_center(&self) -> (Option<f64>, Option<f64>) {
+
+        let center_x = match(self.engine.borrow().get_board().get_cols()) {
+            Some(_) => Some(0.5 * (self.get_left_border() + self.get_right_border())),
+            None => None
+        };
+        let center_y = match(self.engine.borrow().get_board().get_cols()) {
+            Some(_) => Some(0.5 * (self.get_bottom_border() + self.get_top_border())),
+            None => None
+        };
+
+        (center_x, center_y)
     }
 
     fn to_logical(&self, x: f64, y: f64) -> (isize, isize) {
