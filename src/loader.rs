d896fbaf7da5a7b99ee9195df1b24e06a015a981
“risboo6909” <ttyv00@gmail.com>
2017-07-28 18:39:20 +0300

    More tests + better errors

--- src/loader.rs
+++ src/loader.rs
@@ -1,10 +1,13 @@
 extern crate engine;
-
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, Error};
 use std::iter::{FromIterator, Peekable};
 use std::cell::RefCell;
+use std::error;
+use std::fmt;
+use std::fmt::{Formatter, Display};
+
 use engine::Coord;
 
 
@@ -24,6 +27,39 @@ pub enum ParseError {
     EmptyName,
     WrongName(String),
 }
+
+impl Display for ParseError {
+
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+
+        match *self {
+            ParseError::NotANumber(ref unparsed) => write!(f, "Not a number {}", unparsed),
+            ParseError::UnexpectedSymbol(ref unparsed) => write!(f, "Unexpected symbol {}", unparsed),
+            ParseError::InputExhausted => write!(f, "Input exhausted"),
+            ParseError::EmptyName => write!(f, "Option name can't be empty"),
+            ParseError::WrongName(ref unparsed) => write!(f, "Wrong option name {}", unparsed)
+        }
+
+    }
+
+}
+
+impl error::Error for ParseError {
+
+    fn description(&self) -> &str {
+
+        match *self {
+            ParseError::NotANumber(_) => "Not a number",
+            ParseError::UnexpectedSymbol(_) => "Unexpected symbol",
+            ParseError::InputExhausted => "Input exhausted",
+            ParseError::EmptyName => "Option name can't be empty",
+            ParseError::WrongName(_) => "Wrong option name",
+        }
+
+    }
+
+}
+
 
 trait InputProviderTrait {
     fn read_line(&mut self) -> Option<String>;
@@ -257,7 +293,7 @@ fn lexer(line: &str) -> Result<Vec<Lexem>, ParseError> {
                 // read param name
                 let name = get_str(&mut prefix.iter().cloned().peekable());
 
-                // ensure parameter name is ok
+                // ensure parameter name is valid, i.e.
                 if name.is_empty() {
                     // it's not empty
                     return Err(ParseError::EmptyName);
@@ -400,5 +436,21 @@ fn test_lexer_error3() {
 
 #[test]
 fn test_parse_rle1() {
-    println!("{:?}", from_string(String::from("x =  3\ny = 0\nbo$2bo$3o!")).unwrap());
+    // glyder
+    assert!(from_string(String::from("x =  3, y = 0\nbo$2b\no$3o!")).unwrap() ==
+    [Coord { col: 1, row: 0 }, Coord { col: 2, row: 1 }, Coord { col: 0, row: 2 }, Coord { col: 1, row: 2 }, Coord { col: 2, row: 2 }]);
+}
+
+#[test]
+fn test_parse_rle2() {
+    // Gosper glider gun
+    assert!(from_string(String::from("x= 20, y=100\n24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4bobo$10bo5bo7bo$11bo3bo$12b2o!")).unwrap() ==
+           &[Coord { col: 24, row: 0 }, Coord { col: 22, row: 1 }, Coord { col: 24, row: 1 }, Coord { col: 12, row: 2 }, Coord { col: 13, row: 2 },
+             Coord { col: 20, row: 2 }, Coord { col: 21, row: 2 }, Coord { col: 34, row: 2 }, Coord { col: 35, row: 2 }, Coord { col: 11, row: 3 },
+             Coord { col: 15, row: 3 }, Coord { col: 20, row: 3 }, Coord { col: 21, row: 3 }, Coord { col: 34, row: 3 }, Coord { col: 35, row: 3 },
+             Coord { col: 0, row: 4 }, Coord { col: 1, row: 4 }, Coord { col: 10, row: 4 }, Coord { col: 16, row: 4 }, Coord { col: 20, row: 4 },
+             Coord { col: 21, row: 4 }, Coord { col: 0, row: 5 }, Coord { col: 1, row: 5 }, Coord { col: 10, row: 5 }, Coord { col: 14, row: 5 },
+             Coord { col: 16, row: 5 }, Coord { col: 17, row: 5 }, Coord { col: 22, row: 5 }, Coord { col: 24, row: 5 }, Coord { col: 10, row: 6 },
+             Coord { col: 16, row: 6 }, Coord { col: 24, row: 6 }, Coord { col: 11, row: 7 }, Coord { col: 15, row: 7 }, Coord { col: 12, row: 8 },
+             Coord { col: 13, row: 8 }][..]);
 }
